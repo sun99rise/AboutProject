f85d79b2351f6435ec65aa4da56d0883c4c7dcc7
Suraj99r <suraj.rathod17@vit.edu>
2021-06-07 20:34:33 +0530

    updated

--- src/main/java/com/main/AboutProjectApplication.java
+++ src/main/java/com/main/AboutProjectApplication.java
@@ -3,7 +3,7 @@ package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class AboutProjectApplication {
 
 	public static void main(String[] args) {
